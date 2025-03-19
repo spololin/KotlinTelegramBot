2306874526565b8dd3978b2f57577dde5b6c756a
Пололин Сергей Александрович <spololin@sportmaster.ru>
2025-03-19 07:38:51 +0300

    KTB-14 parsing updateId and message with use regex

--- src/main/kotlin/Telegram.kt
+++ src/main/kotlin/Telegram.kt
@@ -7,23 +7,29 @@ import java.net.http.HttpResponse
 
 fun main(args: Array<String>) {
     val botToken = args[0]
-
-    var updateId = 0
+    var updateId: Int? = 0
+    val updateIdRegex: Regex = "\"update_id\":(\\d+),".toRegex()
+    val messageTextRegex: Regex = "\"text\":\"(.+)\"".toRegex()
 
     while (true) {
         Thread.sleep(2000)
         val updates: String = getUpdates(botToken, updateId)
         println(updates)
 
-        val startUpdateId = updates.lastIndexOf("update_id")
-        val endUpdateId = updates.lastIndexOf(",\n\"message\"")
-        if (startUpdateId == -1 || endUpdateId == -1) continue
-        val updateIdString = updates.substring(startUpdateId + 11, endUpdateId)
-        updateId = updateIdString.toInt() + 1
+        val updateIdMatchResult: MatchResult? = updateIdRegex.find(updates)
+        val updateIdGroups = updateIdMatchResult?.groups
+        updateId = updateIdGroups?.get(1)?.value?.toInt()?.plus(1)
+        if (updateId == null) continue
+
+        val matchResult: MatchResult? = messageTextRegex.find(updates)
+        val groups = matchResult?.groups
+        val text = groups?.get(1)?.value
+
+        println(text)
     }
 }
 
-fun getUpdates(botToken: String, updateId: Int): String {
+fun getUpdates(botToken: String, updateId: Int?): String {
     val urlGetUpdates = "https://api.telegram.org/bot$botToken/getUpdates?offset=$updateId"
     val client: HttpClient = HttpClient.newBuilder().build()
     val request: HttpRequest = HttpRequest.newBuilder().uri(URI.create(urlGetUpdates)).build()
